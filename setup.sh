8b19af9ed8aaeeaa659c0f22d3a135fed4b8bd59
garrettdreyfus <garrettdreyfus@gmail.com>
2014-02-20 01:29:33 -0500

    Added Nginx ?
    I added Nginx but untested

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+# GIT 
+###########################################
 echo Repository name:
 read projectname
 #create Project repo
@@ -31,3 +33,33 @@ git pull hub master
 
 exec git-update-server-info" >> /var/git/$projectname.git/hooks/post-update
 chmod +x /var/git/$projectname.git/hooks/post-update
+
+
+
+
+#Nginx
+##################################################################
+apt-get install nginx
+service nginx start
+mkdir -p /var/www/logs/$projectname
+touch /var/www/logs/$projectname/access.log
+touch /var/www/logs/$projectname/error.log
+echo "
+server {
+    listen 80;
+    root /var/www/"$projectname";
+    access_log /var/www/logs/"$projectname"/access.log;
+    error_log /var/www/logs/"$projectname"/error.log;
+    location / {
+        proxy_set_header X-Forward-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $http_host;
+        proxy_redirect off;
+        if (!-f $request_filename) {
+            proxy_pass http://127.0.0.1:8000;
+            break;
+        }
+    }
+}" >> /etc/nginx/sites-available/$projectname.conf
+ln -s /etc/nginx/sites-available/$projectname.conf /etc/nginx/sites-enabled/
+nginx -t
+service nginx reload
